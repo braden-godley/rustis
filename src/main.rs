60ddde4898ec88f22fc1595f7e51564c63d9eb96
Braden Godley <braden@bgodley.com>
2024-04-30 00:49:39 -0700

    start implementing server/client

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,28 @@
 mod server;
 
+use clap::{arg, command, value_parser, ArgAction, Command};
+
 fn main() {
+    let matches = command!()
+        .subcommand(
+            Command::new("server")
+                .about("Runs the server")
+                .arg(arg!(-t --threads <N> "number of tcp threads").default_value("1000"))
+                
+        )
+        .subcommand(
+            Command::new("client")
+                .about("Runs a client that will connect to the server")
+        )
+        .get_matches();
+
+    if let Some(matches) = matches.subcommand_matches("server") {
+        let threads: usize = matches
+            .get_one::<String>("threads").expect("required")
+            .parse::<usize>().expect("Invalid thread number");
+
+        server::start_server(threads);
+    } else if let Some(matches) = matches.subcommand_matches("client") {
+        println!("You just ran the client!");
+    }
 }
--- src/server.rs
+++ src/server.rs
@@ -1,31 +1,81 @@
+use rustis::threadpool::ThreadPool;
+use rustis::pubsub::PubSub;
+use std::net::{TcpStream, TcpListener};
+use std::io::{prelude::*, BufReader};
+use std::sync::Arc;
 
-fn start_server() {
+struct ServerState {
+    ps: PubSub,
+}
+
+pub fn start_server(threads: usize) {
+    println!("Starting server with {threads} threads!");
+
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     let ps = PubSub::new();
-    let http_pool = ThreadPool::new(2000);
-    
+    let tcp_pool = ThreadPool::new(threads);
+
+    let state = Arc::new(&mut ServerState { ps });
+
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        pool.execute(|| {
-            handle_connection(stream);
+        let state = Arc::clone(&state);
+
+        tcp_pool.execute(move || {
+            handle_connection(stream, state);
         });
     }
 }
 
-fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
-    let _http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+enum RustisCommand {
+    Subscribe,
+    Publish,
+    Unknown,
+}
 
+fn handle_connection(mut stream: TcpStream, state: Arc<&mut ServerState>) {
+    let mut buf_reader = BufReader::new(&mut stream);
 
-    let contents = fs::read_to_string("hello.html").unwrap();
-    let length = contents.len();
+    let mut buf = String::new();
+    while let Ok(_) = buf_reader.read_line(&mut buf) {
+        let components: Vec<&str> = buf.split("|").collect();
 
-    let response = 
-        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+        use RustisCommand::*;
 
-    stream.write_all(response.as_bytes()).unwrap();
+        let command = match *components.first().unwrap() {
+            "subscribe" => Subscribe,
+            "publish" => Publish,
+            _ => Unknown,
+        };
+
+        let response = match command {
+            Subscribe => {
+                "success"
+            },
+            Publish => {
+                let channel = components.get(1);
+                let message = components.get(2);
+
+                if channel.is_some() && message.is_some() {
+                    state.ps.publish(channel.unwrap().to_string(), message.unwrap().to_string());
+                    "success"
+                } else {
+                    "invalid"
+                }
+            },
+            Unknown => {
+                "unknown command"
+            },
+        };
+
+        stream.write_all(response.as_bytes()).unwrap();
+    }
+
+    // let contents = fs::read_to_string("hello.html").unwrap();
+    // let length = contents.len();
+    //
+    // let response = 
+    //     format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    //
+    // stream.write_all(response.as_bytes()).unwrap();
 }
